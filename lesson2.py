7db6059231338c57872b81cf480c87fc20e2a59c
revaza1andria <matejaniashvili12@gmail.com>
2025-02-03 15:11:22 +0000

    first commit

---
+++ lesson2.py
@@ -0,0 +1,16 @@
+name = "me var andria" #ეს არის str(string) ტიპის ცვლადი
+surname = "revazashvili"
+age = "13 wlis"  #ეს არის int (integer) მთელი რიცხვი
+height = "chemi simage aris daaxloeit 160cm"  #ეს არის float ტიპის ცვლადი (ათწილადი)
+
+print(name) #
+
+
+knows_programming = "true" #true or false
+is_ugly = "false" #snakecaaase(უბრალობ წერის სტილი სტანდარტულად)
+
+isUgly = "false" #ჯავასკრიპტული camelcase
+
+print(name + " " + surname)
+
+print (  name + " " +  str(surname) + " " + str(age) + " " + str(height))
